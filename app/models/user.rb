096d104583b331aa889ab2656ef46aa193b95a38
Jeff <fjeffschmidt@gmail.com>
2010-11-12 10:50:35 -0500

    Added a signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,8 +38,13 @@ class User < ActiveRecord::Base
   
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
-    return nil  if user.nil?
-    return user if user.has_password?(submitted_password)
+    if user.nil?
+      nil
+    elsif user.has_password?(submitted_password)
+      user
+    else
+      nil
+    end
   end
 
   private
